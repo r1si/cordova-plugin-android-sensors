10c5183f93bb9dc8b97162fca83542b2e578b09e
Elia Baragiola <elia@dkremoto.it>
2016-12-27 01:11:50 +0100

    Upgrate to 1.0.3 fix bug

--- www/an_sensors.js
+++ www/an_sensors.js
@@ -1,8 +1,8 @@
 var exec = require('cordova/exec');
 
 exports.isDeviceLock = function(success, error) {
-	exec(success, error, "Hello", "isDeviceLock");
+	exec(success, error, "an_sensors", "isDeviceLock");
 };
 exports.isCallActive = function(success, error) {
-	exec(success, error, "Hello", "isCallActive");
+	exec(success, error, "an_sensors", "isCallActive");
 };
